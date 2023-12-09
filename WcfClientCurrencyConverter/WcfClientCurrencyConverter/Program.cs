5eb2277ad5352ef6295c54410a2e51a0be57728b
YasaShrestha <yasashrestha2010@gmail.com>
2023-12-09 14:34:12 +0000

    wrote some code to take a input from the user and convert it using the services and give the output.

---
+++ WcfClientCurrencyConverter/WcfClientCurrencyConverter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfClientCurrencyConverter.ServiceReference1;
+
+namespace WcfClientCurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client client = new Service1Client();
+            // Ener arguments from console.
+            Console.Write("Enter the GBP to convert into Indian, Nepalese, Pakistani and Srilankan currency : ");
+            double gbp = Convert.ToDouble(Console.ReadLine());
+            
+            // Call the Add service operation.
+            double inrValue = client.ConvertToInr(gbp);
+            Console.WriteLine("\nIndian value is:{0} ", inrValue);
+
+            double nprValue = client.ConvertToNpr(gbp);
+            Console.WriteLine("\nNepalese rupees value is:{0} ", nprValue);
+
+            double pkrValue = client.ConvertToPkr(gbp);
+            Console.WriteLine("\nPakistani value is:{0} ", pkrValue);
+
+            double slValue = client.ConvertToSl(gbp);
+            Console.WriteLine("\nSrilankan value is:{0} ", slValue);
+
+            Console.WriteLine("\nPress <ENTER> to terminate client.");
+            Console.ReadLine();
+
+
+            //Closing the client releases all communication resources.
+            client.Close();
+
+        }
+    }
+}
